b60a7eb51c9c8fb8a6f48789641a93cff1b4aae7
aleadr <aleadr@gmail.com>
2021-02-27 22:32:39 +0700

    add expressjs and mui

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const { createServer } = require("http");
+//const { createServer } = require("http");
+const express = require('express');
 const next = require("next");
 
 const port = process.env.PORT || 3000;
@@ -6,6 +7,25 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+app.prepare()
+  .then(() => {
+    const server = express();
+    
+    server.get('*', (req, res) => {
+			return handle(req, res);
+		});
+
+		server.listen(port, err => {
+			if (err) throw err;
+			console.log('> Ready on http://localhost:' + port);
+		});
+  })
+  .catch(err => {
+    console.log(JSON.stringify(err))
+    process.exit(1)
+  })
+
+/*
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
@@ -14,3 +34,4 @@ app.prepare().then(() => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
+*/
